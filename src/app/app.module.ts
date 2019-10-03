50aeb66245c9fde270c9e551ef367b372807d822
juliana_zagurska <zagurska1@gmail.com>
2019-10-03 22:55:17 +0300

    output the list of items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import {FormsModule} from '@angular/forms'; //NgModel
+import { FormsModule } from '@angular/forms'; //NgModel
 
 @NgModule({
   declarations: [
